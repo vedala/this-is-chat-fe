507355d1b01de427ad1c9942f2179820403ec3dd
Kalyan Vedala <svedala23@gmail.com>
2025-11-11 07:43:34 -0800

    Basic form and display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  const listMessages = messages.map(message => <li>{message}</li>)
   function submitMessage(formData) {
     const inputMessage = formData.get("input-msg");
     console.log("submitMessage: ", inputMessage);
+    const newMessages = [...messages, inputMessage];
+    setMessages(newMessages);
   }
 
   return (
@@ -11,10 +17,13 @@ function App() {
       <header className="App-header">
         Welcome to This-Is-Chat
       </header>
-      <form id="input-form" action={submitMessage}>
-        <input id="input-msg" name="input-msg" autoComplete="off" />
-        <button>Send</button>
-      </form>
+      <div id="messages-window">
+        <ul>{listMessages}</ul>
+        <form id="input-form" action={submitMessage}>
+          <input id="input-msg" name="input-msg" autoComplete="off" />
+          <button>Send</button>
+        </form>
+      </div>
     </div>
   );
 }
