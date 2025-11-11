c4d452441441c4964125b16e72f576bce9eb995e
Kalyan Vedala <svedala23@gmail.com>
2025-11-11 07:31:36 -0800

    Setup form/input and add css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
 function App() {
+  function submitMessage(formData) {
+    const inputMessage = formData.get("input-msg");
+    console.log("submitMessage: ", inputMessage);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Welcome to This-Is-Chat
       </header>
+      <form id="input-form" action={submitMessage}>
+        <input id="input-msg" name="input-msg" autoComplete="off" />
+        <button>Send</button>
+      </form>
     </div>
   );
 }
