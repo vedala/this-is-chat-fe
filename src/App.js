365f9a7abd2ced96ac3ce62a25d9e03efd624142
Kalyan Vedala <svedala23@gmail.com>
2025-12-06 06:47:37 -0800

    Reorganize App and index

--- src/App.js
+++ src/App.js
@@ -1,107 +1,31 @@
-import { useState, useEffect, useRef } from 'react';
-import './App.css';
-import axios from 'axios';
-import { useAuth0 } from '@auth0/auth0-react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
+import './App.css';
+import Landing from "./Landing";
+import Home from "./Home";
 
 function App() {
-  const [userId, setUserId] = useState(null);
-  const [messages, setMessages] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const bottomRef = useRef(null);
-  const ws = useRef(null);
-  const { user } = useAuth0();
-
-console.log("user=", user);
-  const listMessages = messages.map((row) => {
-    const isMine = row.userId === userId;
-
-    return (
-      <div
-        key={row._id}
-        className={`message ${isMine ? "mine" : "theirs"}`}
-      >
-        {row.message}
-      </div>
-    );
-  });
-
-  useEffect(() => {
-    let id = sessionStorage.getItem("userId");
-    if (!id) {
-      id = crypto.randomUUID();
-      sessionStorage.setItem("userId", id);
-    }
-
-    setUserId(id);
-  }, []);
-
-  useEffect(() => {
-    async function fetchData() {
-      const url = new URL("messages", process.env.REACT_APP_CHAT_API_URL);
-      const response = await axios.get(url);
-      setMessages(response.data);
-    };
-
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    ws.current = new WebSocket(process.env.REACT_APP_CHAT_API_URL);
-
-    ws.current.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-
-      setMessages((prev) => [...prev, data.message]);
-    };
-
-    return () => ws.current.close();
-  }, []);
-
-  useEffect(() => {
-    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
-
-  async function submitMessage(event) {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const inputMessage = formData.get("input-msg");
-
-    if (!inputMessage.trim()) return;
-
-    ws.current.send(
-      JSON.stringify({
-        text: inputMessage,
-        userId: userId
-      })
-    );
-
-    setInputValue("");
-  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        This-Is-Chat
-      </header>
-      <div className="messages-window">
-        <div className="message-list">
-          {listMessages}
-          <div ref={bottomRef}></div>
-        </div>
-        <form className="input-form" onSubmit={submitMessage}>
-          <input
-            id="input-msg"
-            name="input-msg"
-            autoComplete="off"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/"
+            element={<Landing />}
           />
-          <button>Send</button>
-        </form>
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+        </Routes>
       </div>
-    </div>
+    </Router>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,29 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { BrowserRouter } from 'react-router-dom';
 
 const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
 const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const AUTH0_REDIRECT_URI = process.env.REACT_APP_AUTH0_REDIRECT_URI;
 const AUTH0_AUDIENCE = process.env.REACT_APP_AUTH0_AUDIENCE;
 
-
+console.log("AUTH0_REDIRECT_URI=", AUTH0_REDIRECT_URI);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Auth0Provider
-        domain={AUTH0_DOMAIN}
-        clientId={AUTH0_CLIENT_ID}
-        authorizationParams={{
-          redirect_uri: AUTH0_REDIRECT_URI,
-          audience: AUTH0_AUDIENCE
-        }}
-      >
-        <App />
-      </Auth0Provider>
-    </BrowserRouter>
+    <Auth0Provider
+      domain={AUTH0_DOMAIN}
+      clientId={AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: AUTH0_REDIRECT_URI,
+        audience: AUTH0_AUDIENCE
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
